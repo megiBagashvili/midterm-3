27920a710985aa31b138f593e2d81657e303dcc6
megiBagashvili <megibagashvili2004@gmail.com>
2025-12-07 22:07:48 +0400

    feat:setup app configuration, global pipes, and common DTOs

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // Assuming .env has MONGO_URL, or hardcoding for local dev if .env not present
+    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/hw-project'),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/common/dto/is-valid-object-id.dto.ts
@@ -0,0 +1,6 @@
+import { IsMongoId } from "class-validator";
+
+export class IsValidObjectId {
+    @IsMongoId()
+    id!: string
+}
